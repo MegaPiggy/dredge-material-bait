1ee7a85421c68aaf8c2dc211be81b7b8e1dba3eb
xen-42 <nicholasconnors@hotmail.ca>
2024-08-24 17:13:21 -0400

    Initial commit

---
+++ MaterialBait/MaterialBait.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Winch.Core;
+
+namespace MaterialBait
+{
+	public class MaterialBait : MonoBehaviour
+	{
+		public void Awake()
+		{
+			WinchCore.Log.Debug($"{nameof(MaterialBait)} has loaded!");
+		}
+	}
+}
