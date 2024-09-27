736154d06d132b2bfc8d5587eccd3b2ad58e63e1
xen-42 <nicholasconnors@hotmail.ca>
2024-09-27 00:29:56 -0400

    Wasn't unused

---
+++ MaterialBait/Enums.cs
@@ -0,0 +1,13 @@
+﻿using Winch.Util;
+
+namespace MaterialBait;
+
+
+[EnumHolder]
+public static class Enums
+{
+    /// <summary>
+    /// Appears to be unused, but is required for the bait-recipe-grid json file
+    /// </summary>
+    public static readonly GridKey BAIT_MATERIAL_RECIPE;
+}
